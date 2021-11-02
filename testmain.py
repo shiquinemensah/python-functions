bf702a523d85410d0117315e17392094c651075e
shiquine <godchildshiquine@gmail.com>
2021-11-02 05:42:27 -0700

    functions ]

---
+++ testmain.py
@@ -0,0 +1,12 @@
+def sayhi(name):
+    print('hello ' + name ) # to create a function you would need to use 'def' and assign  it a value
+    print('hello' + name + ', you are' + str(age))
+                        # when that has been created python naturally creates an indentation // so anything within that
+                        # section will be added to the function. To call upon a function you would need write the value
+                        # that you assigned 'def'
+print('top')
+sayhi('mike')                 #< This is an example of how to call upon a function
+sayhi('mike', '35')
+print('bottom')         # function s should be named with all lower case
+
+
